4d350b3355da1764f098a05570c86b7ce89d02ea
JDtheGeek <john.dennehy@gmail.com>
2019-11-19 12:59:04 +0000

    replaces #amount with attr_reader for insance variable - tests still pass

--- transaction.rb
+++ transaction.rb
@@ -1,13 +1,10 @@
 
 class Transaction
 
+  attr_reader :amount
+
   def initialize(amount:)
     @amount = amount
   end
 
-  def amount
-    return -100 if @amount == -100
-    return 100 if @amount == 100
-    return 200 if @amount == 200
-  end
 end
