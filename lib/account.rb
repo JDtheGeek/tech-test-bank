318e7de3a0e979c31d54ee8e7d4c0a6e2dcabd53
JDtheGeek <john.dennehy@gmail.com>
2019-11-19 16:00:57 +0000

    adds #balance to Account, and returns 0 balance

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,5 @@
 class Account
-
+  def balance
+    0
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,5 +2,9 @@ require 'rspec'
 require 'account'
 
 describe Account do
+  it 'new account has a balance of 0' do
+    account = Account.new
+    expect(account.balance).to eq 0
+  end
 
 end
