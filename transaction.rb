98af90c6ddf90c4e8b527a4bf0eb05968d005b04
JDtheGeek <john.dennehy@gmail.com>
2019-11-19 14:19:36 +0000

    refactor with rubocop

--- transaction.rb
+++ transaction.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 require 'date'
 class Transaction
-
   attr_reader :amount, :transaction_type, :date
 
   def initialize(amount:, date: Date.today)
@@ -10,9 +11,9 @@ class Transaction
   end
 
   private
-  def check_transaction_type
-    @transaction_type = 'credit' if amount > 0
-    @transaction_type = 'debit' if amount < 0
-  end
 
+  def check_transaction_type
+    @transaction_type = 'credit' if amount.positive?
+    @transaction_type = 'debit' if amount.negative?
+  end
 end
