ba4569a2eafd41c6a9f9e263a9d0d9b458a730e7
JDtheGeek <john.dennehy@gmail.com>
2019-11-19 12:40:05 +0000

    adds instance varaible to Transaction class to inform amount returned

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -12,4 +12,10 @@ describe Transaction do
     expect(deposit.amount).to eq 100
   end
 
+  it 'records the amount 100 when instantiated with a 100' do
+    deposit = Transaction.new(amount: 200)
+    expect(deposit.amount).to eq 200
+  end
+
+
 end
--- transaction.rb
+++ transaction.rb
@@ -2,10 +2,11 @@
 class Transaction
 
   def initialize(amount:)
-
+    @amount = amount
   end
 
   def amount
-    100
+    return 100 if @amount == 100
+    return 200 if @amount == 200
   end
 end
