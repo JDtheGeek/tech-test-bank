fedaf96b2d33a1f30c39c763481a743f20e7e2a7
JDtheGeek <john.dennehy@gmail.com>
2019-11-20 11:38:08 +0000

    refactors and uncomments tests temporerally commented out during refactor

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -23,33 +23,39 @@ describe Statement do
     expect(print_out).to include('1000.00 |') # deposit
   end
 
-  # it 'can send generate a statement showing debit on 10/01/2012' do
-  #   statement = Statement.new
-  #   print_out = statement.print
-  #   expect(print_out).to include(header)
-  #   expect(print_out).to include('| 10/01/2012')
-  #   expect(print_out).to include('1000.00 |') # deposit
+  it 'can send generate a statement showing debit on 13/01/2012' do
+    transaction2 = instance_double(
+      'Transaction',
+      amount: 2000,
+      type: 'credit',
+      date: Date.new(2012, 1, 13)
+    )
 
-  #   # expect(print_out).not_to include('| 13/01/2012')
-  # end
+    transaction_data = [transaction2]
+    statement = Statement.new(transaction_data)
+    print_out = statement.print
+    expect(print_out).to include(header)
+    expect(print_out).to include('| 13/01/2012')
+    expect(print_out).to include('2000.00 |') # deposit
 
-  # it 'can send generate a statement showing debit on 13/01/2012' do
-  #   statement = Statement.new
-  #   print_out = statement.print
-  #   expect(print_out).to include(header)
-  #   expect(print_out).to include('| 13/01/2012')
-  #   expect(print_out).to include('2000.00 |') # deposit
+    expect(print_out).not_to include('| 10/01/2012')
+  end
 
-  #   # expect(print_out).not_to include('| 10/01/2012')
-  # end
+  it 'can send generate a statement showing withdrawel on 14/01/2012' do
+    transaction2 = instance_double(
+      'Transaction',
+      amount: 500,
+      type: 'debit',
+      date: Date.new(2012, 1, 14)
+    )
 
-  # it 'can send generate a statement showing withdrawel on 14/01/2012' do
-  #   statement = Statement.new
-  #   print_out = statement.print
-  #   expect(print_out).to include(header)
-  #   expect(print_out).to include('| 14/01/2012')
-  #   expect(print_out).to include('500.00 |') # withdrawel
+    transaction_data = [transaction2]
+    statement = Statement.new(transaction_data)
+    print_out = statement.print
+    expect(print_out).to include(header)
+    expect(print_out).to include('| 14/01/2012')
+    expect(print_out).to include('500.00 |') # withdrawel
 
-  #   # expect(print_out).not_to include('| 10/01/2012')
-  # end
+    expect(print_out).not_to include('| 10/01/2012')
+  end
 end
